a5dfab98f2eb344bf7b3562979e0c668955d8195
abdallahleandro <abdallahleandrodev@gmail.com>
2026-08-15 18:40:46 -0300

    Retomndo para pagina anterior.

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,7 +1,20 @@
+import { useParams } from "react-router";
+import { useNavigate } from "react-router";
+
 export function Details() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
   return (
     <div>
+      <button type="button" onClick={() => navigate(-1)}>
+        Voltar
+      </button>
       <h1>Detalhes</h1>
+
+      <span>
+        ID do Produto: <strong>{id}</strong>
+      </span>
     </div>
   );
 }
